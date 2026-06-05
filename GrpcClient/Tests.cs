93b9c700ebbc9d06412899df70e8e83afec97b8e
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2026-06-05 04:55:11 +0100

    Attempt simple cloning

--- GrpcClient/Tests.cs
+++ GrpcClient/Tests.cs
@@ -3,6 +3,7 @@ using GrpcService.SharedCode;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using ProtoBuf.Grpc.ClientFactory;
+using ProtoBuf.Grpc.Configuration;
 
 namespace GrpcClient;
 
@@ -30,5 +31,36 @@ public class Tests
 
         Assert.Equal("Hello world", reply.Message);
         Assert.NotEqual(default, reply.DateTimeOffset);
+    }
+
+    [Fact]
+    public void ObtainSerializer()
+    {
+        var app = new WebApplicationFactory<Program>();
+
+        var clientServiceCollection = new ServiceCollection();
+        clientServiceCollection
+            .AddProtobufSurrogates()
+            .AddCodeFirstGrpcClient<IGreeterService>(nameof(IGreeterService), config =>
+            {
+                config.Address = app.Server.BaseAddress;
+                config.ChannelOptionsActions.Add(option => option.HttpHandler = app.Server.CreateHandler());
+            });
+
+        using var clientServiceProvider = clientServiceCollection.BuildServiceProvider();
+
+        var binder = clientServiceProvider.GetRequiredService<BinderConfiguration>();
+        var reply = new HelloReply
+        {
+            DateTimeOffset = DateTimeOffset.UtcNow,
+            Message = "Test"
+        };
+
+        var marshaller = binder.GetMarshaller<HelloReply>();
+
+        var bytes = marshaller.Serializer(reply);
+        var clone = marshaller.Deserializer(bytes);
+
+        Assert.Equivalent(reply, clone);
     }
 }
